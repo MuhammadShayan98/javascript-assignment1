1cd2b26e7767395635546851016d12ac0d812b8b
Shayan0342 <shayanlodhi0342@gmail.com>
2020-06-15 23:17:17 +0500

    javascript-assignment1 Git

---
+++ app.js
@@ -0,0 +1,405 @@
+// Chaper 1 
+// Question 1
+
+// alert ( "hello world" ); 
+// Question 2
+
+// alert ( "Error!please enter the correct password" )
+
+// Question 3
+
+//       alert ( "Welcome to JS Land..." +"\n"+"happy coding");
+
+//     Question 4
+
+//     alert ( "Welcome to JS Land...");
+//     alert ( "Happy coding");
+
+//     Question 5
+
+//     alert ( "Hello.. I can run JS through console browser");
+    
+//     Chaper 2
+// Question 1
+
+//      var username;
+//     Question 2
+
+//      var myName="Muhammad Shayan";
+
+//     Question 3
+
+//      var message="Hello World";
+//     alert("message");
+
+//     Question 4
+
+//      var name ="jhone doe";
+//     var age="15 year old";
+//     var cert="Certified mobile application development";
+//     alert("name");
+//     alert("age");
+//     alert("cert");
+    
+//     Question 5
+
+    // var name="pizza,\n pizz,\n piz,\n pi,\n p";
+    // alert(name);
+    
+// Question 6
+// var email="My email address is "+ "example@example.com ";
+// alert(email);
+
+// Question 7
+
+// var book="A smarter way to learn JavaScript";
+// alert(book);
+
+// Question 8
+
+// Document.write("yeah!I can write Html through js");
+
+// Question 9
+
+// Chaper 3
+// Question 1
+
+// var age=22;
+// alert("I am "+age+" years old");
+
+// Question 2
+//  var N=14;
+//  alert("you have visited this site "+N+ "times");
+
+// Question 3
+//  var birthYear=1990;
+//  alert("My birth year is "+birthYear);
+
+// Question 4
+// var VisitoName="Jone Doe";
+// var productTitle="T-shirts";
+// var quantity=5;
+// document.write(VisitoName + "ordered "+quantity+ " "+productTitle +" on XYZ Clothing store");
+
+// Chapter 4
+// Question 1
+
+// var a,b,c;
+
+// Question 2
+
+// legal
+// var name;
+// var NAME;
+// var fullName;
+// var last_name;
+// var  name6;
+
+// illegal
+//  var 6name;
+//  var full name;
+//  var last.name;
+//  var #name;
+//  var name@;
+
+// Question 3
+
+// document.write(<h1>Rules for naming JS variables</h1>);
+// var a="number";
+// var b="$";
+// var c="_";
+// var d="letter";
+// var e="sensitive";
+// var f="keyword";
+// document.write("Variable names can only contain " +a+ " " +b+" and "+c);
+// document.write("Variable must begin with" +d + " "+ b +" and " +c);
+// document.write("Variable names can are case"+d);
+// document.write("Variable names shouls not be Js"+f);
+
+// Chapter 5
+// Quesion 1
+
+// var a=5,b=6;
+// c=a+b;
+// document.write( "the sum 5 and 3 is ",c);
+
+// Quesion 2
+
+// var a=4;
+// var b=5;
+// var c=a+b;
+// var d=a-b;
+// var e=a/b;
+// var f=a%b;
+
+// Quesion 3
+
+//  var a;
+//  document.write(a);
+//  a=5;
+//  document.write("intial value ",a);
+//  b=a++;
+//  document.write("increment ",b);
+//  c=a--;
+//  document.write("decrement ",a);
+//  d=c+7;
+//  document.write("add 7 ",d);
+//  e=--d;
+//  document.write("again decrement ",e);
+//  f=e%3;
+//  document.write("remainder is",f); 
+
+// Question 4
+
+// var ticket =600;
+// var a=ticket * 5;
+// document.write("the cost of buying 5 tickets is ",a);
+
+// Chapter 6-9
+
+// Question
+
+// document.write("Results");
+// var a=10;
+// ddocument.write("the value of ++ a is ",b); 
+// document.write("now the value of a is ",b);
+// var b=++a;
+// document.write("the value of ++a is ",b); 
+// document.write("now the value of a is ",b); 
+// var c=--a;
+// document.write("the value of ++a is ",c); 
+// document.write("now the value of a is ",c);
+
+// var d=a--;
+// document.write("the value of  a-- is ",b); 
+// document.write("now the value of a is ",b);
+
+// Question 2
+
+// var a=2,b=1,e,f,i,h;
+// d=--a;    //1
+// document.write("the value of  --a ",d); 
+// e=--b;  //0
+// f=d-e; //1-0=1
+// document.write("the value of a - --b ",f); 
+// e=++b; //2
+// g=d-f+e; //1-1+2=2
+// document.write("the value of  --a - --b + ++b ",g ); 
+// i=b--;
+// h=d-f+e+i; //1-1+2+0=2
+// document.write("the value of  --a - --b + ++b + b-- ",h); 
+
+// Question 3
+// var a;
+// prompt("user name is",a);
+// prompt("Welcome to world of programming");
+
+// Question 4
+
+// var a,b,c;
+// prompt( a);
+// prompt( b);
+// c=a*b;
+// document
+
+// chapter 9-11
+
+// Question 1
+
+// var cityName;
+// prompt(cityName);
+// if(cityName="karachi"){
+//     document.write("Welcome to karachi");
+// }
+// else{
+//     document.write("nothing");
+// }
+
+// Question 2
+
+// var gender;
+// prompt(gender);
+// if(gender="male"){
+//     document.write("Good morning sir");
+// }
+
+// Question 3
+
+// var input=prompt(input);
+// if(input==="red"){
+//     document.write("stop");
+
+//     if(input==="yellow"){
+//         document.write("ready to move")
+//     }
+//         if(input==="green"){
+//         document.write(" move now")
+//     }
+// }
+
+// else{
+
+//         document.write("nothing")
+    
+// }
+
+// Question 4
+
+// var fuel=prompt();
+// if(fuel< 0.25){
+// document.write("Please refill the fuel in your car");
+// }
+
+
+// Question 5
+
+// a) var a = 4;
+// if (++a === 5) // true
+
+// alert("given condition for variable a is true");}
+
+// b) var b = 82; //true
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+//c)
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+
+// d) var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals"); tue
+// }
+
+// Chapter 12-13
+
+// Question 1
+// var char=input();
+
+// if(char <= 65 && char >=95 ){
+// document.write("uper case");
+// }
+// else if(char <= 97 && char >=122 ){
+//     document.write("lower case");
+//     }
+// else{
+//     document.write("nothing");
+// }
+
+// Question 2
+//  var a=prompt();
+//  var b=prompt();
+//   if(a > b){
+//       document.write("a is larger than b");
+//       if(a===b){
+//         document.write("equal");
+//       }
+//   }
+//   else{
+//     document.write("b is larger than a");
+//   }
+
+// Question 3
+
+// var num=prompt();
+// if(num > 0 ){
+//     document.write("positive");
+// }
+//    else if(num==0){
+//         document.write("zero");
+//     }
+//    else if(num < 0){
+//         document.write("negative");
+//     }
+
+// Question 4
+
+// var vow=prompt();
+// if( vow=="a" || vow=="e" || vow=="i" || vow=="o" || vow=="u" ||vow=="A" || vow=="E" || vow=="I" || vow=="O" || vow=="U" )
+// {
+//     document.write("vowel");  
+// }
+// else{
+//     document.write("false");
+// }
+
+// chapter 14 -16
+
+// Question 1
+
+// var names[]=;
+
+// Question 2
+
+// var names=[];
+
+// Question 3
+
+// var names=["a"];
+
+ // Question 4
+
+// var names=[1];
+
+// Question 5
+
+// var names=[true];
+
+// Question 6
+
+// var names=[true,"abc",1];
+
+// Question 7
+// var names = ["HSC <br/>" , "BCS <br/>" , "BS <br/>", "BCOM <br/>","MS <br/>", "M.Phil <br/>", "PhD","<br/>"];// document.write(names);
+
+// chaper 17-20
+
+// Question 1
+
+// var a=[[]];
+
+// Question 2
+
+//  var a=[[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+ 
+// Question 3
+
+// var a;
+// for(a=1;a<=10;a++){
+//     document.write(a+"<br/>");}
+// 
+
+// Question 4
+
+// var i=prompt("Enter the number table ") ;
+// var e=prompt("Enter the length multiplication") ;
+// for(var a=1;a<=e;a++){
+//   document.write(i*a+"<br/>");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
